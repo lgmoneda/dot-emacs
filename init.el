f77aa21dd8c63278ce427edfd00cec11652560f7
lgmoneda <lg.moneda@gmail.com>
2017-01-30 00:22:20 -0200

    elpy

--- init.el
+++ init.el
@@ -5,7 +5,9 @@
 
 (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                          ("marmalade" . "https://marmalade-repo.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")))
+                         ("melpa" . "https://melpa.org/packages/")
+			 ("elpy" . "<http://jorgenschaefer.github.io/packages/>")))
+
 
 
 (add-to-list 'load-path "~/.emacs.d/elpa/neotree-20170110.321")
@@ -34,3 +36,39 @@
 
 ;Turn the system sound off
 (setq ring-bell-function 'ignore)
+
+(require 'cl)
+(setq cfg-var: packages '(
+		elpy
+		flycheck
+		company
+))
+
+(defun cfg:install-packages ()
+	(let ((pkgs (remove-if #'package-installed-p cfg-var:packages)))
+		(when pkgs
+			(message "%s" "Emacs refresh packages database&#x2026;")
+			(package-refresh-contents)
+			(message "%s" " done.")
+			(dolist (p cfg-var:packages)
+				(package-install p)))))
+(package-initialize)
+(cfg:install-packages)
+
+
+;;; Enabling Elpy
+(elpy-enable)
+(remove-hook 'elpy-modules 'elpy-module-highlight-indentation)
+(setq elpy-rpc-python-command "python3")
+(setq python-shell-interpreter "python3")
+(setq elpy-rpc-backend "jedi")
+
+(add-hook 'python-mode-hook
+		  (lambda ()
+			(setq-default indent-tabs-mode nil)
+			(setq-default tab-width 4)
+			(setq-default python-indent 4)))
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
