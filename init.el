7172c734955e288c7052fb467278e7a2a75c644c
lgmoneda <lg.moneda@gmail.com>
2017-01-31 17:35:04 -0200

    beacon

--- init.el
+++ init.el
@@ -11,7 +11,6 @@
 			 ("elpy" . "<http://jorgenschaefer.github.io/packages/>")))
 
 
-
 (add-to-list 'load-path "~/.emacs.d/elpa/neotree-20170110.321")
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
@@ -26,7 +25,20 @@
 (global-set-key [C-tab] 
     (lambda ()
       (interactive)
-      (other-window -1)))
+      (other-window 1)))
+
+
+;Defining switch franes command
+(global-set-key (kbd "C-2")
+    (lambda ()
+      (interactive)
+      (other-frame 1)))
+
+;Defining switch franes command
+(global-set-key (kbd "C-1")
+    (lambda ()
+      (interactive)
+      (bury-buffer)))
 
 ;Initialize in full screen
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
@@ -40,38 +52,7 @@
 ;Turn the system sound off
 (setq ring-bell-function 'ignore)
 
-(require 'cl)
-(setq cfg-var: packages '(
-		elpy
-		flycheck
-		company
-))
-
-(defun cfg:install-packages ()
-	(let ((pkgs (remove-if #'package-installed-p cfg-var:packages)))
-		(when pkgs
-			(message "%s" "Emacs refresh packages database&#x2026;")
-			(package-refresh-contents)
-			(message "%s" " done.")
-			(dolist (p cfg-var:packages)
-				(package-install p)))))
-(package-initialize)
-(cfg:install-packages)
-
-
-;;; Enabling Elpy
-(elpy-enable)
-(remove-hook 'elpy-modules 'elpy-module-highlight-indentation)
-(setq elpy-rpc-python-command "python3")
-(setq python-shell-interpreter "python3")
-(setq elpy-rpc-backend "jedi")
-
-(add-hook 'python-mode-hook
-		  (lambda ()
-			(setq-default indent-tabs-mode nil)
-			(setq-default tab-width 4)
-			(setq-default python-indent 4)))
-
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;Beacon minor mode
+(add-to-list 'load-path "~/.emacs.d/elpa/beacon-20161004.756")
+(require 'which-key)
+(which-key-mode)
