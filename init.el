cc9a7a5f21b77826182e90054b104f5eb6f134cf
lgmoneda <lg.moneda@gmail.com>
2018-05-29 09:23:48 -0300

    comment erc part

--- init.el
+++ init.el
@@ -987,129 +987,99 @@ if breakpoints are present in `python-mode' files"
 ;;============================
 ;;==          ERC           ==
 ;;============================
-(add-to-list 'load-path "~/.emacs.d/elisp/erc-extras" t)
+;; (add-to-list 'load-path "~/.emacs.d/elisp/erc-extras" t)
 
-;; Trying to display nicely
-(erc-spelling-mode 1)
-(add-hook 'erc-mode-hook (lambda () (auto-fill-mode 0)))
-(make-variable-buffer-local 'erc-fill-column)
- (add-hook 'window-configuration-change-hook 
-	   '(lambda ()
-	      (save-excursion
-	        (walk-windows
-		 (lambda (w)
-		   (let ((buffer (window-buffer w)))
-		     (set-buffer buffer)
-		     (when (eq major-mode 'erc-mode)
-		       (setq erc-fill-column (- (window-width w) 2)))))))))
-
-
-(when (assoc "en0" (network-interface-list))
-  (erc :server "irc.freenode.net" :port 6667 :nick "lgmoneda"))
+;; ;; Trying to display nicely
+;; (erc-spelling-mode 1)
+;; (add-hook 'erc-mode-hook (lambda () (auto-fill-mode 0)))
+;; (make-variable-buffer-local 'erc-fill-column)
+;;  (add-hook 'window-configuration-change-hook 
+;; 	   '(lambda ()
+;; 	      (save-excursion
+;; 	        (walk-windows
+;; 		 (lambda (w)
+;; 		   (let ((buffer (window-buffer w)))
+;; 		     (set-buffer buffer)
+;; 		     (when (eq major-mode 'erc-mode)
+;; 		       (setq erc-fill-column (- (window-width w) 2)))))))))
 
 
-(progn
-     (require 'erc)
-     (require 'erc-track)
-     (erc-track-mode +1)
+;; (when (assoc "en0" (network-interface-list))
+;;   (erc :server "irc.freenode.net" :port 6667 :nick "lgmoneda"))
 
-     ;; keywords to track
-     (setq erc-keywords '("bayes" "reinforcement" "unsupervised" "bayesian"))
 
-     ;; Ignore Server Buffer
-     (setq erc-track-exclude-server-buffer t)
+;; (progn
+;;      (require 'erc)
+;;      (require 'erc-track)
+;;      (erc-track-mode +1)
 
-     ;; show only when my nickname is mentioned in any channel
-     (setq erc-current-nick-highlight-type 'nick)
-     (setq erc-track-exclude-types '("JOIN" "PART" "QUIT" "NICK" "MODE"))
-     (setq erc-track-use-faces t)
-     (setq erc-track-faces-priority-list
-           '(erc-current-nick-face
-             erc-keyword-face
-             erc-direct-msg-face))
-     (setq erc-track-priority-faces-only 'all))
+;;      ;; keywords to track
+;;      (setq erc-keywords '("bayes" "reinforcement" "unsupervised" "bayesian"))
 
-(load "~/.ercfile")
-(require 'erc-services)
-(erc-services-mode 1)
-(setq erc-prompt-for-nickserv-password nil)
-(setq erc-nickserv-passwords
-      `((freenode (("lgmoneda" . ,lgmonedanick)))))
+;;      ;; Ignore Server Buffer
+;;      (setq erc-track-exclude-server-buffer t)
 
-;; Prevents Erc buffers flashing at start
-(erc-autojoin-mode t)
-(setq erc-autojoin-timing :ident)
-(setq erc-autojoin-delay 40)
-(setq erc-join-buffer 'bury)
-(setq erc-autojoin-channels-alist
-      '(("freenode.net" "#emacs" "#sptk" "##machinelearning"
-	 "#scikit-learn" "#tensorflow")))
+;;      ;; show only when my nickname is mentioned in any channel
+;;      (setq erc-current-nick-highlight-type 'nick)
+;;      (setq erc-track-exclude-types '("JOIN" "PART" "QUIT" "NICK" "MODE"))
+;;      (setq erc-track-use-faces t)
+;;      (setq erc-track-faces-priority-list
+;;            '(erc-current-nick-face
+;;              erc-keyword-face
+;;              erc-direct-msg-face))
+;;      (setq erc-track-priority-faces-only 'all))
 
-(erc-autojoin-after-ident "irc.freenode.net" "lgmoneda")
+;; (load "~/.ercfile")
+;; (require 'erc-services)
+;; (erc-services-mode 1)
+;; (setq erc-prompt-for-nickserv-password nil)
+;; (setq erc-nickserv-passwords
+;;       `((freenode (("lgmoneda" . ,lgmonedanick)))))
 
-(add-hook 'erc-nickserv-identified-hook 'erc-autojoin-after-ident)
+;; ;; Prevents Erc buffers flashing at start
+;; (erc-autojoin-mode t)
+;; (setq erc-autojoin-timing :ident)
+;; (setq erc-autojoin-delay 40)
+;; (setq erc-join-buffer 'bury)
+;; (setq erc-autojoin-channels-alist
+;;       '(("freenode.net" "#emacs" "#sptk" "##machinelearning"
+;; 	 "#scikit-learn" "#tensorflow")))
 
-;; Log in a buffer when people talk to me
-(setq erc-log-matches-flag t)
-(setq erc-log-matches-types-alist
-          '((keyword . "### Keywords Log ###")
-            (current-nick . "### Me Log ###")))
+;; (erc-autojoin-after-ident "irc.freenode.net" "lgmoneda")
 
-;; (setq erc-keywords '("keras" "bayes" "bayesian" "causality" "reinforcement"))
+;; (add-hook 'erc-nickserv-identified-hook 'erc-autojoin-after-ident)
 
-;; ;; Erc-tracking
-;; (require 'erc-track)
-;; (erc-track-mode t)
+;; ;; Log in a buffer when people talk to me
+;; (setq erc-log-matches-flag t)
+;; (setq erc-log-matches-types-alist
+;;           '((keyword . "### Keywords Log ###")
+;;             (current-nick . "### Me Log ###")))
 
-;; ;; Exclude not interesting messages
-;; ;; Blue for keyword
-;; (setq erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"))
-;; 				;; "324" "329" "332" "333" "353" "477"))
+;; ;; Prevent the new created buffer from pvt to be brought visible
+;; (setq erc-auto-query 'bury)
 
-;; ;; Ignore Server Buffer
-;; (setq erc-track-exclude-server-buffer t)
+;; ;; Both defadvices to track pvts using channels mode-line face
+;; (defadvice erc-track-find-face (around erc-track-find-face-promote-query activate)
+;;   (if (erc-query-buffer-p) 
+;;       (setq ad-return-value (intern "erc-current-nick-face"))
+;;     ad-do-it))
 
-;; ;; That fixes the modeline notification for nick mention
-;; (setq erc-current-nick-highlight-type 'nick)
+;; (defadvice erc-track-modified-channels (around erc-track-modified-channels-promote-query activate)
+;;   (if (erc-query-buffer-p) (setq erc-track-priority-faces-only 'nil))
+;;   ad-do-it
+;;   (if (erc-query-buffer-p) (setq erc-track-priority-faces-only 'all)))
 
-;; (setq erc-track-priority-faces-only 'all)
+;; (require 'erc-nicklist)
+;; ;; bk's toggle nicklist!
+;; (defun bk/nicklist-toggle ()
+;;   "Function to toggle the nicklist in ERC mode."
+;;   (interactive)
+;;   (let ((nicklist-buffer-name (format " *%s-nicklist*" (buffer-name))))
+;;     (if (get-buffer nicklist-buffer-name)
+;;         (kill-buffer nicklist-buffer-name)
+;;       (erc-nicklist))))
 
-;; ;; Track only mentions and keywords
-;; (setq erc-track-use-faces t)
-;; ;;(setq erc-format-query-as-channel-p t)
-
-;; ;;Show pvt, current-nick and keyword
-;; (setq erc-track-faces-priority-list
-;;       '(erc-current-nick-face
-;; 	erc-keyword-face
-;; 	erc-direct-msg-face))
-
-;;(setq erc-query-display 'bury)
-;; Prevent the new created buffer from pvt to be brought visible
-(setq erc-auto-query 'bury)
-
-;; Both defadvices to track pvts using channels mode-line face
-(defadvice erc-track-find-face (around erc-track-find-face-promote-query activate)
-  (if (erc-query-buffer-p) 
-      (setq ad-return-value (intern "erc-current-nick-face"))
-    ad-do-it))
-
-(defadvice erc-track-modified-channels (around erc-track-modified-channels-promote-query activate)
-  (if (erc-query-buffer-p) (setq erc-track-priority-faces-only 'nil))
-  ad-do-it
-  (if (erc-query-buffer-p) (setq erc-track-priority-faces-only 'all)))
-
-(require 'erc-nicklist)
-;; bk's toggle nicklist!
-(defun bk/nicklist-toggle ()
-  "Function to toggle the nicklist in ERC mode."
-  (interactive)
-  (let ((nicklist-buffer-name (format " *%s-nicklist*" (buffer-name))))
-    (if (get-buffer nicklist-buffer-name)
-        (kill-buffer nicklist-buffer-name)
-      (erc-nicklist))))
-
-(define-key erc-mode-map (kbd "<f7>") 'bk/nicklist-toggle)
+;; (define-key erc-mode-map (kbd "<f7>") 'bk/nicklist-toggle)
 
 ;; Smarter beep
 ;; Remember to apt-get install mplayer!
@@ -2265,12 +2235,13 @@ Whenever a journal entry is created the
       (terminal-notifier-notify "Emacs Notification" msg)
       (message msg)))
 
-(remind-me-daily 'reminder-fn "8:00am" "Leave home!" "bike-horn.wav" t)
-(remind-me-daily 'reminder-fn "3:00pm" "You're hungry!" nil t)
-(remind-me-daily 'reminder-fn "6:00pm" "You're hungry!" nil t)
-(remind-me-daily 'reminder-fn "8:00pm" "Go home!" "bike-horn.wav" t)
+(remind-me-daily 'reminder-fn "8:01am" "Leave home!" nil t)
+(remind-me-daily 'reminder-fn "3:01pm" "You're hungry!" nil t)
+(remind-me-daily 'reminder-fn "6:01pm" "You're hungry!" nil t)
+(remind-me-daily 'reminder-fn "8:01pm" "Go home!" nil t)
 (remind-me-daily 'reminder-fn "8:30pm" "Dinner!" nil t)
 (remind-me-daily 'reminder-fn "11:40pm" "Bedtime!" nil t)
+
 
 (defun my-terminal-notifier-notify (info)
   "Show a message with terminal-notifier-command."
@@ -2296,4 +2267,6 @@ Whenever a journal entry is created the
 (setq alert-default-style 'style-name)
 
 
+;; Node connection
+(load "~/Dropbox/Projetos/Emacs/.nu-node.el")
 
