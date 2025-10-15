99b01953ecdfe4eededb3108cf86d6ed43f60218
luis-moneda <lg.moneda@gmail.com>
2025-10-15 10:52:53 -0300

    clean

--- settings/elisp-settings.el
+++ settings/elisp-settings.el
@@ -1,22 +1,5 @@
 ;;; elisp-settings.el --- Settings for the programming language Python
 
-;; Fast jump to elisp function
-;; In Debian derived distros, the package emacs24 or similar
-;; does not include the source elisp code, you need it to
-;; jump to the definition (.el or .el.gz files). To download it
-;; install emacs24-el package.
-(defun lgm/describe-func ()
-  (interactive)
-  (describe-function (function-called-at-point)))
-
-(defun lgm/jump-to-elisp-func-def ()
-  (interactive)
-  (find-function (function-called-at-point))
-  )
-
-(global-set-key (kbd "C-h C-j") 'lgm/jump-to-elisp-func-def)
-(global-set-key (kbd "C-h C-f") 'lgm/describe-func)
-
 ;; Rainbow delimiters in Elisp mode
 (use-package rainbow-delimiters
   :ensure t
